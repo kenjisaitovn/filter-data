e04be3482eb55b769c30153a4e59d133a3cc5a52
HaiHT <haiht369@gmail.com>
2017-01-23 00:03:37 +0700

    add auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,8 +1,13 @@
 <?php
-Route::get('/', 'RequestallController@index');
-Route::post('/filter', 'RequestallController@filter');
-Route::get('/list-fb', 'RequestallController@listFb');
-Route::get('/list-query', 'RequestallController@listQuery');
-Route::get('/copy-table-requestall-to-new-table', 'RequestallController@copyTblRequestAllToNewTbl');
-Route::get('/saveLastStateOfTblRequesAllCopy', 'RequestallController@saveLastStateOfTblRequesAllCopy');
+Route::group(['middleware' => ['web','auth']], function () {
+    Route::get('/', 'RequestallController@index');
+    Route::post('/filter', 'RequestallController@filter');
+    Route::get('/list-fb', 'RequestallController@listFb');
+    Route::get('/list-query', 'RequestallController@listQuery');
+    Route::get('/copy-table-requestall-to-new-table', 'RequestallController@copyTblRequestAllToNewTbl');
+    Route::get('/saveLastStateOfTblRequesAllCopy', 'RequestallController@saveLastStateOfTblRequesAllCopy');
+});
 
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
