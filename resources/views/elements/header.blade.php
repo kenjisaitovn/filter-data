5e0dae0a018d88b8ca1a1e6d048e6dd2f0f5ff04
HaiHT <haiht369@gmail.com>
2017-01-23 08:36:20 +0700

    change font

--- resources/views/elements/header.blade.php
+++ resources/views/elements/header.blade.php
@@ -9,8 +9,6 @@
 
 <title>Index</title>
 
-<!-- Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 <!-- Styles -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
